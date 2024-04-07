c4c2b62f7228e96ac4dcf3cf8e96c5e4b68f3963
umair <muhammadumair4018@gmail.com>
2024-04-07 17:40:50 +0500

    useEffect updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,19 @@ import {  useSelector } from "react-redux";
 import ComponentRoute from "./Routes";
 import Api from "./services/api";
 import Loader from "./common/loader";
-import { useNavigate } from "react-router-dom"; 
 const App = () => {
     const token = useSelector((state) => state.login.user?.token);
     const [loading, setLoading] = useState(true);
-    const navigate=useNavigate();
-  useEffect(() => {
-    const fetchToken = async () => {
-      if (token) {
-        Api.setClientToken(token);
-        navigate("/dashboard"); // Navigate to "/" if the token exists
-      } else {
-        navigate("/login"); // Navigate to "/main" if the token doesn't exist
-      }
-      setLoading(false);
-    };
-    fetchToken();
-  }, [token, history]);
+    useEffect(() => {
+      const fetchToken = async () => {
+        if (token) {
+          Api.setClientToken(token);
+          setLoading(false);
+        }
+      };
+      fetchToken();
+    }, [token]);
+    
     if (loading) {
         return <Loader />;
     }
