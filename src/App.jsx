70457171022dce650be622b37946d79d08b6a8a6
umair <muhammadumair4018@gmail.com>
2024-04-08 12:04:51 +0500

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
 import React, { useEffect, useState } from "react";
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Api from "./services/api";
 import Loader from "./common/loader";
 import ComponentRoute from "./Routes";
+
 const App = () => {
     const token = useSelector((state) => state.login.user?.token);
     const [loading, setLoading] = useState(true);
-    const navigate=useNavigate();
-    useEffect(() => {
-      const fetchToken = async () => {
-        if (token) {
-          Api.setClientToken(token);
-          navigate("/")
+    const navigate = useNavigate();
 
-          setLoading(false);
-        }
-        else{
-          navigate("/login")
-        }
-      };
-      fetchToken();
-    }, [token]);
-    
-  
+    useEffect(() => {
+        const fetchToken = async () => {
+            if (token) {
+                Api.setClientToken(token);
+                navigate("/");
+            } else {
+                navigate("/login");
+            }
+            setLoading(false);
+        };
+        fetchToken();
+    }, [token, navigate]);
+
+    // Render loading spinner while checking token
+    if (loading) {
+        return <Loader />;
+    }
+
+    // Once loading is done, render the component routes
     return (
         <>
-        <ComponentRoute/>
+            <ComponentRoute />
         </>
     );
 };
-export default App;
 
+export default App;
